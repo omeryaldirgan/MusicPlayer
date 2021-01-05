01c4730270d38db21a5fd9c6ec1222da2fe591a1
omeryaldirgan <omeryaldirgan@hotmail.com>
2021-01-05 22:33:07 +0300

    expo av add
    Müzik paketi entegre edildi

--- components/PlayerWidget/index.tsx
+++ components/PlayerWidget/index.tsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {View,Text,Image} from 'react-native'
 import styles from './styles'
 import {AntDesign, FontAwesome} from "@expo/vector-icons";
-
+import { Audio, Video } from 'expo-av';
 const song={
     id:'1',
+    uri:'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4',
     imageUri:'https://i.scdn.co/image/ab67616d0000b27396384c98ac4f3e7c2440f5b5',
     title:'High on You',
-    artist:'Helen'
+    artist:'Vadim'
 }
 
 const PlayerWidget=()=>{
+     const [sound, setSound] = useState<Sound|null>(null)
+      const onPlaybackStatusUpdate=(status)=>{
+         console.log(status);
+      }
+    const playCurrentSong=async ()=>{
+        const { sound } = Sound.createAsync(
+            { uri: song.uri },
+            { shouldPlay: true },
+             onPlaybackStatusUpdate
+        )
+        setSound(sound)
+    }
+
+    useEffect(()=>{
+        playCurrentSong();
+    },[]);
+
     return(
         <View style={styles.container}>
             <Image source={{uri:song.imageUri}} style={styles.image}/>
